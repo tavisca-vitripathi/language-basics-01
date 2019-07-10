92c9aa779d306f6e6e834a853be6a239e852cf2e
tavisca-vitripathi <tripathi.t@yahoo.com>
2019-07-10 19:23:11 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+    
+using System;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -22,7 +23,59 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+          
+             //splitting the given equation string
+            string firstArg = equation.Split('*')[0];
+            string secondArg = equation.Split('*')[1].Split('=')[0];
+            string result = equation.Split('=')[1];
+
+            //conditions to fulfill:
+            //  1. No Integer value possible
+            //  2. size is not equal(leading zeros)
+            //  3. else return missing one digit
+
+
+            //checking which one is faulty
+            bool flag_firstArg = Int32.TryParse(firstArg, out int int_firstArg);
+            bool flag_secondArg = Int32.TryParse(secondArg, out int int_secondArg);
+            bool flag_result = Int32.TryParse(result, out int int_result);
+
+            
+            string updatedEquation = "";
+
+            if(flag_firstArg == false){
+                if(int_result % int_secondArg==0){                              //condtion 1
+                    string temp = (int_result / int_secondArg) + "";
+                    updatedEquation += temp + "*" + secondArg + "=" +result;
+                }else 
+                    return -1;
+            }
+
+            if(flag_secondArg == false){
+                if(int_result % int_firstArg == 0){
+                string temp = (int_result / int_firstArg) + "";
+                updatedEquation += firstArg + "*" + temp + "=" +result;
+                }else
+                    return -1;
+            }
+
+            if(flag_result == false){
+                string temp = (int_firstArg * int_secondArg) + "";
+                updatedEquation += firstArg + "*" + secondArg + "=" +temp;
+            }
+
+
+            //condition 2
+            if (equation.Length != updatedEquation.Length){
+                return -1;
+	        }else{
+                //condition 3
+                for (int i = 0; i < updatedEquation.Length; i++)
+			    {
+                    if(updatedEquation[i] != equation[i] && equation[i]=='?')
+                        return (int)(updatedEquation[i] - '0');
+			    }
+            }
             throw new NotImplementedException();
         }
     }
